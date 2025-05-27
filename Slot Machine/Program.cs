02e5789da42bca3129c7ddf0e77f0b3be6d956c6
Fu888UK <fu83man@hotmail.co.uk>
2025-05-27 11:44:23 +0100

    made a start with new slot machine project the for loop in the if was just copied across from previous project, next step to amend this

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int ONELINE = 1;
+            const int TWOLINES = 2;
+            const int THREELINES = 3;
+            const int ROW = 3;
+            const int COL = 3;
+
+
+            Console.WriteLine($"Hello, please select the amount you wish to play with, {ONELINE} for $1, {TWOLINES} for $2, {THREELINES} for $3");
+            string wager = Console.ReadLine();
+
+            Console.WriteLine($"You have selected {wager}");
+
+            int selected = int.Parse(wager);
+
+            int[,] grid = new int[ROW,COL];
+
+            Random rnd = new Random();
+
+            if (selected == 1) 
+            {
+                for (int i = 0; i < grid.GetLength(0); i++) 
+                {
+                    for (int j = 0; j < grid.GetLength(1); j++) 
+                    {
+                        grid[i, j] = (char)('0' + rnd.Next(0, 9));
+                    }
+                }
+
+
+            }
+            if (selected == 2)
+            {
+
+
+            }
+            if (selected == 3)
+            {
+
+
+            }
+
+
+
         }
     }
 }

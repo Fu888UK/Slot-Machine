7ad4b2de46df8aa9971a8e5a1ff000570dc761eb
Fu888UK <fu83man@hotmail.co.uk>
2025-05-29 20:19:49 +0100

    populated and printed grid need to clarify my modes (middle, horizontal, diagonal, vertical ), then use these for 'if', and also check results

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -7,43 +7,70 @@
             const int ONELINE = 1;
             const int TWOLINES = 2;
             const int THREELINES = 3;
+            const int THREELINES = 4;
             const int ROW = 3;
             const int COL = 3;
 
-
+            Console.WriteLine("Welcome to the Slots Machine");
             Console.WriteLine($"Hello, please select the amount you wish to play with, {ONELINE} for $1, {TWOLINES} for $2, {THREELINES} for $3");
-            string wager = Console.ReadLine();
+            //modes middle, horizontal, diagonal, vertical 
+            string mode = Console.ReadLine();
+                       
 
-            Console.WriteLine($"You have selected {wager}");
+            int selected = int.Parse(mode);                         //convert string to int 
+            Console.WriteLine($"You have selected {selected}");
 
-            int selected = int.Parse(wager);
+            int[,] grid = new int[ROW,COL];                         //2D array 
 
-            int[,] grid = new int[ROW,COL];
+            Random rnd = new Random();                       
 
-            Random rnd = new Random();
-
-            if (selected == 1) 
+            //populate grid 
+            for (int i = 0; i < ROW; i++)                           //row
             {
-                for (int i = 0; i < grid.GetLength(0); i++) 
+                for (int j = 0; j < COL; j++)                       //col
                 {
-                    for (int j = 0; j < grid.GetLength(1); j++) 
-                    {
-                        grid[i, j] = (char)('0' + rnd.Next(0, 9));
-                    }
+                    grid[i, j] = rnd.Next(0, 5);
+                }             
+            }
+            //print grid
+            //Console.WriteLine("");
+            for (int i = 0; i < ROW; i++)                           //row
+            {
+                for (int j = 0; j < COL; j++)                       //col
+                {
+                    Console.Write(grid[i, j] + " ");
                 }
-
-
+                Console.WriteLine();                                //need this in the loop to output like a grid
             }
-            if (selected == 2)
-            {
+
+            //check grid/winnings 
+
+            //incorporate above to ifs below
+            //need to calify modes, is this do do with amount of wager, the difficulty of the game, ??????
 
 
-            }
-            if (selected == 3)
-            {
+            //if (selected == 1) 
+            //{
+            //    for (int i = 0; i < 3; i++) 
+            //    {
+            //        for (int j = 0; j < 3; j++) 
+            //        {
+            //            grid[i, j] = (char)('0' + rnd.Next(0, 9));
+            //        }
+            //    }
 
 
-            }
+            //}
+            //if (selected == 2)
+            //{
+
+
+            //}
+            //if (selected == 3)
+            //{
+
+
+            //}
 
 
 

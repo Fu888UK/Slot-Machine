c04415d322e181af47ebda9da7d392d28b156720
Fu888UK <fu83man@hotmail.co.uk>
2025-06-24 15:49:04 +0100

    have move wthe majority of the code/game within the while loop, similar to previous hangman project, also as discussed in previous LC will be testing and experimenting now, to make sure the code works as it did before. Assuming this works fine, then I need to add the bet and winning amounts to the code, but have noticed that if i change the value of variable 'balance' then the code doen'nt behave as I would like

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -16,169 +16,302 @@ namespace Slot_Machine
             const int COLS = 3;
             const int PAYOUT1 = 6;
             const int PAYOUT2 = 0;
-            int balance = 50;
+            int balance = 0;
             int betAmount = 3;
 
 
             Console.WriteLine("Welcome to the Slots Machine");
-
-            while (balance > 0) 
-            {
-                Console.WriteLine($"your current balance is {balance}");
-                Console.WriteLine("How much do you want to bet");
-                if (balance == 0) 
-                {
-                    Console.WriteLine("Sorry, you do not have any credit left to continue");
-                }
-                if (betAmount > balance) 
-                {
-                    Console.WriteLine("you do not have enough in your balance for that bet");
-                }
-
-            }
-            
             Console.WriteLine($"Please select the mode you wish to play");
             Console.WriteLine($"{MIDLINE} = Middle line");
             Console.WriteLine($"{HORILINES} = Horizontal lines");
             Console.WriteLine($"{VERTLINES} = Verticle lines");
             Console.WriteLine($"{DIAGLINES} = Diagonal lines");
-                                    
-            //if users enters anything apart from 1 2 3 4 need to enter 'if' to fix this, refer to 1st project
 
-            string mode = Console.ReadLine();                       
+            string mode = Console.ReadLine();
 
             int selectedMode = int.Parse(mode);                         //convert string to int 
-            
-            Console.WriteLine($"You have selected {selectedMode} with a bet amount of {betAmount}");
 
+            Console.WriteLine($"You have selected {selectedMode}");
             if (selectedMode > 4)               //if users enters anything apart from 1 2 3 4 need to enter 'if' to fix this, refer to 1st project
             {
                 Console.WriteLine("Invalid selection try again");
                 return;
             }
+            Console.WriteLine($"Your bet amount is {betAmount}");
+                       
 
-            int[,] grid = new int[ROWS,COLS];                         //2D array 
-
-            Random rnd = new Random();
-
-            //create test grid by hard coding
-            //int[,] testGrid = new int[3, 3]
-            //{
-            //    {0,1,2},
-            //    {1,1,0},
-            //    {0,1,2}
-            //};
-
-            //populate grid 
-            for (int i = 0; i < ROWS; i++)                           //row
+            while (balance > 0)
             {
-                for (int j = 0; j < COLS; j++)                       //col
+                Console.WriteLine($"your current balance is {balance}");
+                Console.WriteLine("How much do you want to bet");
+                                              
+
+                int[,] grid = new int[ROWS, COLS];                         //2D array 
+
+                Random rnd = new Random();
+
+                //create test grid by hard coding
+                //int[,] testGrid = new int[3, 3]
+                //{
+                //    {0,1,2},
+                //    {1,1,0},
+                //    {0,1,2}
+                //};
+
+                //populate grid 
+                for (int i = 0; i < ROWS; i++)                           //row
                 {
-                    grid[i, j] = rnd.Next(0, 5);
-                }
-            }
-            //print grid
-            Console.WriteLine("");
-
-            for (int i = 0; i < ROWS; i++)                           //row
-            {
-                for (int j = 0; j < COLS; j++)                       //col
-                {
-                    Console.Write(grid[i, j] + " ");
-                }
-                Console.WriteLine();                                //need this in the loop to output like a grid
-            }
-            Console.WriteLine();                       
-
-            bool win = true;                                //check grid/winnings 
-
-            if (selectedMode == MIDLINE)
-            {                                        
-                int midRow = ROWS / 2;
-
-                for (int i = 1; i < COLS; i++)
-                {                 
-                    //check 1,0 against 1,1 and 1,2, if 1,0 and 1,1 is wrong once this is checked then no need to check 1,2
-                    if (grid[midRow, 0] != grid[midRow, i]) //&& grid[1, i] == grid[1, i])
+                    for (int j = 0; j < COLS; j++)                       //col
                     {
-                        win = false;
-                        Console.WriteLine("middle row lose");
-                        break;
-                    }                    
-                }                
-            }
-            if (selectedMode == HORILINES)
-            {
-                for (int i = 0; i < ROWS; i++)                                   //loops through all 3 rows 
+                        grid[i, j] = rnd.Next(0, 5);
+                    }
+                }
+                //print grid
+                Console.WriteLine("");
+
+                for (int i = 0; i < ROWS; i++)                           //row
                 {
-                    for (int j = 0; j < COLS; j++) 
+                    for (int j = 0; j < COLS; j++)                       //col
                     {
-                        if (grid[i, 0] != grid[i, j]) //&& grid[i, 1] == grid[i, 2])   //checking for values are the same                                                   
+                        Console.Write(grid[i, j] + " ");
+                    }
+                    Console.WriteLine();                                //need this in the loop to output like a grid
+                }
+                Console.WriteLine();
+
+                bool win = true;                                //check grid/winnings 
+
+                if (selectedMode == MIDLINE)
+                {
+                    int midRow = ROWS / 2;
+
+                    for (int i = 1; i < COLS; i++)
+                    {
+                        //check 1,0 against 1,1 and 1,2, if 1,0 and 1,1 is wrong once this is checked then no need to check 1,2
+                        if (grid[midRow, 0] != grid[midRow, i]) //&& grid[1, i] == grid[1, i])
                         {
                             win = false;
-                            Console.WriteLine($"You lose on horizontal line {i + 1}!");
+                            Console.WriteLine("middle row lose");
                             break;
-                        }                    
-                    }                    
-                }                
-            }
-            if (selectedMode == VERTLINES)
-            {
-                for (int i = 0; i < COLS; i++)                                   //loops through all 3 rows 
+                        }
+                    }
+                }
+                if (selectedMode == HORILINES)
                 {
-                    for (int j = 0; j < ROWS; j++) 
-                    {                        
-                        if (grid[j, i] != grid[0, i])         //checking for values are the same                                                   
+                    for (int i = 0; i < ROWS; i++)                                   //loops through all 3 rows 
+                    {
+                        for (int j = 0; j < COLS; j++)
+                        {
+                            if (grid[i, 0] != grid[i, j]) //&& grid[i, 1] == grid[i, 2])   //checking for values are the same                                                   
+                            {
+                                win = false;
+                                Console.WriteLine($"You lose on horizontal line {i + 1}!");
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (selectedMode == VERTLINES)
+                {
+                    for (int i = 0; i < COLS; i++)                                   //loops through all 3 rows 
+                    {
+                        for (int j = 0; j < ROWS; j++)
+                        {
+                            if (grid[j, i] != grid[0, i])         //checking for values are the same                                                   
+                            {
+                                win = false;
+                                Console.WriteLine($"You LOSE on vertical line {i + 1}!");
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (selectedMode == DIAGLINES)              //0,0 1,1 2,2  line i need to find     TOP LEFT TO BOTTOM RIGHT                                         
+                {
+                    //bool winLine1 = true;
+                    for (int i = 0; i < ROWS; i++)                                   //loops through all 3 rows 
+                    {
+                        if (grid[0, 0] != grid[i, i])                            //checking for values are the same                                                   
                         {
                             win = false;
-                            Console.WriteLine($"You LOSE on vertical line {i + 1}!");
+                            Console.WriteLine("You lose on line 1");
                             break;
-                        }                        
-                    }                        
-                }               
-            }
-            if (selectedMode == DIAGLINES)              //0,0 1,1 2,2  line i need to find     TOP LEFT TO BOTTOM RIGHT                                         
-            {
-                //bool winLine1 = true;
-                for (int i = 0; i < ROWS; i++)                                   //loops through all 3 rows 
-                {                    
-                    if (grid[0, 0] != grid[i, i])                            //checking for values are the same                                                   
-                    {
-                        win = false;                        
-                        Console.WriteLine("You lose on line 1");
-                        break;
 
+                        }
+                        //if (win)
+                        //{
+                        //    Console.WriteLine("You win on line 1");
+                        //}
                     }
-                    //if (win)
-                    //{
-                    //    Console.WriteLine("You win on line 1");
-                    //}
+                    //bool winLine2 = true;
+                    for (int i = 0; i < ROWS; i++)             //0,2 1,1 2,0  line i need to find   //TOP RIGHT TO BOTTOM LEFT 
+                    {
+                        if (grid[0, COLS - 1] != grid[i, ROWS - 1 - i])
+                        {
+                            win = false;
+                            Console.WriteLine("You lose on line 2");
+                            break;
+                        }
+                        //if (win)
+                        //{
+                        //    Console.WriteLine("You win on line 2");
+                        //}
+                    }
+
                 }
-                //bool winLine2 = true;
-                for (int i = 0; i < ROWS; i++)             //0,2 1,1 2,0  line i need to find   //TOP RIGHT TO BOTTOM LEFT 
+                else //change to else?
                 {
-                    if (grid[0, COLS - 1] != grid[i, ROWS - 1 - i])    
-                    {
-                        win = false;
-                        Console.WriteLine("You lose on line 2");
-                        break;
-                    }
-                    //if (win)
-                    //{
-                    //    Console.WriteLine("You win on line 2");
-                    //}
+                    Console.WriteLine("You win");
+                    Console.WriteLine("You have won??????????");
+                    //output winnings, need calculation
+                }
+                if (balance == 0)
+                {
+                    Console.WriteLine("Sorry, you do not have any credit left to continue");
+                }
+                if (betAmount > balance)
+                {
+                    Console.WriteLine("you do not have enough in your balance for that bet");
                 }
 
             }
-            else //change to else?
-            {
-                Console.WriteLine("You win");
-                Console.WriteLine("You have won??????????");
-                //output winnings, need calculation
-            }                        
         }
     }
 }
+
+
+
+
+
+                       
+//            if (selectedMode > 4)               //if users enters anything apart from 1 2 3 4 need to enter 'if' to fix this, refer to 1st project
+//            {
+//                Console.WriteLine("Invalid selection try again");
+//                return;
+//            }
+
+//            int[,] grid = new int[ROWS,COLS];                         //2D array 
+
+//            Random rnd = new Random();
+
+//            //create test grid by hard coding
+//            //int[,] testGrid = new int[3, 3]
+//            //{
+//            //    {0,1,2},
+//            //    {1,1,0},
+//            //    {0,1,2}
+//            //};
+
+//            //populate grid 
+//            for (int i = 0; i < ROWS; i++)                           //row
+//            {
+//                for (int j = 0; j < COLS; j++)                       //col
+//                {
+//                    grid[i, j] = rnd.Next(0, 5);
+//                }
+//            }
+//            //print grid
+//            Console.WriteLine("");
+
+//            for (int i = 0; i < ROWS; i++)                           //row
+//            {
+//                for (int j = 0; j < COLS; j++)                       //col
+//                {
+//                    Console.Write(grid[i, j] + " ");
+//                }
+//                Console.WriteLine();                                //need this in the loop to output like a grid
+//            }
+//            Console.WriteLine();                       
+
+//            bool win = true;                                //check grid/winnings 
+
+//            if (selectedMode == MIDLINE)
+//            {                                        
+//                int midRow = ROWS / 2;
+
+//                for (int i = 1; i < COLS; i++)
+//                {                 
+//                    //check 1,0 against 1,1 and 1,2, if 1,0 and 1,1 is wrong once this is checked then no need to check 1,2
+//                    if (grid[midRow, 0] != grid[midRow, i]) //&& grid[1, i] == grid[1, i])
+//                    {
+//                        win = false;
+//                        Console.WriteLine("middle row lose");
+//                        break;
+//                    }                    
+//                }                
+//            }
+//            if (selectedMode == HORILINES)
+//            {
+//                for (int i = 0; i < ROWS; i++)                                   //loops through all 3 rows 
+//                {
+//                    for (int j = 0; j < COLS; j++) 
+//                    {
+//                        if (grid[i, 0] != grid[i, j]) //&& grid[i, 1] == grid[i, 2])   //checking for values are the same                                                   
+//                        {
+//                            win = false;
+//                            Console.WriteLine($"You lose on horizontal line {i + 1}!");
+//                            break;
+//                        }                    
+//                    }                    
+//                }                
+//            }
+//            if (selectedMode == VERTLINES)
+//            {
+//                for (int i = 0; i < COLS; i++)                                   //loops through all 3 rows 
+//                {
+//                    for (int j = 0; j < ROWS; j++) 
+//                    {                        
+//                        if (grid[j, i] != grid[0, i])         //checking for values are the same                                                   
+//                        {
+//                            win = false;
+//                            Console.WriteLine($"You LOSE on vertical line {i + 1}!");
+//                            break;
+//                        }                        
+//                    }                        
+//                }               
+//            }
+//            if (selectedMode == DIAGLINES)              //0,0 1,1 2,2  line i need to find     TOP LEFT TO BOTTOM RIGHT                                         
+//            {
+//                //bool winLine1 = true;
+//                for (int i = 0; i < ROWS; i++)                                   //loops through all 3 rows 
+//                {                    
+//                    if (grid[0, 0] != grid[i, i])                            //checking for values are the same                                                   
+//                    {
+//                        win = false;                        
+//                        Console.WriteLine("You lose on line 1");
+//                        break;
+
+//                    }
+//                    //if (win)
+//                    //{
+//                    //    Console.WriteLine("You win on line 1");
+//                    //}
+//                }
+//                //bool winLine2 = true;
+//                for (int i = 0; i < ROWS; i++)             //0,2 1,1 2,0  line i need to find   //TOP RIGHT TO BOTTOM LEFT 
+//                {
+//                    if (grid[0, COLS - 1] != grid[i, ROWS - 1 - i])    
+//                    {
+//                        win = false;
+//                        Console.WriteLine("You lose on line 2");
+//                        break;
+//                    }
+//                    //if (win)
+//                    //{
+//                    //    Console.WriteLine("You win on line 2");
+//                    //}
+//                }
+
+//            }
+//            else //change to else?
+//            {
+//                Console.WriteLine("You win");
+//                Console.WriteLine("You have won??????????");
+//                //output winnings, need calculation
+//            }                        
+//        }
+//    }
+//}
 
 //need to add bet amount/how many lines 
 

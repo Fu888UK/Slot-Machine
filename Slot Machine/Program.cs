fa642eb555c541e09d359808b88793353ab8d171
Fu888UK <fu83man@hotmail.co.uk>
2025-07-03 19:53:21 +0100

    corrected magic numbers written TryParse line, next step to re-structure this whithin a 'While'

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -17,10 +17,11 @@ namespace Slot_Machine
             const int COLS = 3;
             const int PAYOUT = 3;
             const int GRID_MAX = 5;
-            const int GRID_MIN = 0;       
-
-            int INITIAL_BALANCE = 50;   //saved value
+            const int GRID_MIN = 0;
+            const int INT_BALANCE = 50;
             const int BET_AMOUNT = 3;
+            int balance = INT_BALANCE;   //saved value
+            int betAmmount = BET_AMOUNT;
 
             Console.WriteLine("Welcome to the Slots Machine");
             Console.WriteLine($"Please select the mode you wish to play");
@@ -29,34 +30,47 @@ namespace Slot_Machine
             Console.WriteLine($"{VERTLINES} = Verticle lines");
             Console.WriteLine($"{DIAGLINES} = Diagonal lines");
 
-            Console.WriteLine($"Your starting balance is {INITIAL_BALANCE}");
+            Console.WriteLine($"Your starting balance is {balance}");
             Console.WriteLine($"Your default bet amount is {BET_AMOUNT}");
 
-            while (INITIAL_BALANCE > 0)
+            while (balance > 0)
             {
                 string mode = Console.ReadLine();
+                while () 
+                {
 
-                int selectedMode = int.Parse(mode);  //tryParse - look at link sent on whatsapp                       //convert string to int                       
+                }
+                if (!int.TryParse(mode, out int selectedMode))      //TryParse(String, int32) Method signature
+                                                                    //TryParse(mode, selectedMode) Method signature
+                                                                    //Purpose - to convert string representation of a number to its 32-bit int equivalent
+                                                                    //returns either true if conversion is succeeded, or false if failed 
+                                                                    //string or mode (in my case - input string to convert 
+                                                                    //out int restult - output variable that receives the converted int (in my case selectedMode)
+                                                                    //out - get the parsed value while checking success
 
-                if (selectedMode > INVALID)               //if users enters anything apart from 1 2 3 4 need to enter 'if' to fix this, refer to 1st project
+                    //1- parse 'mode' (user input) to int.
+                    //2- if successful 'selectedMode gets parsed to int val. Then returns true (but if !true/false it skips the 'if' block???
+                    //3- if failed 'selectedMode' gets 0. Then returns false (but if !false/true it enters the 'if' block
+
+                    //bool selectedMode = int.TryParse(mode, out selectedMode);  //tryParse - look at link sent on whatsapp          //convert string to int
+                    //int selectedMode = int.Parse(mode);
+
+                    //if i press enter without chosing a mode, the outcome wins?????
+
+                    if (selectedMode > INVALID)               
                 {
                     Console.WriteLine("Invalid selection please try again with a valid selection");
                     return;
                 }
                 Console.WriteLine($"You have selected {selectedMode}");
-                INITIAL_BALANCE = INITIAL_BALANCE - BET_AMOUNT;  //updated value
-                Console.WriteLine($"Your balance is now {INITIAL_BALANCE}");
-                
-
-                //Console.WriteLine($"your current balance is {balance}");
-                //Console.WriteLine("How much do you want to bet");
-                if (BET_AMOUNT > INITIAL_BALANCE)
+                balance = balance - BET_AMOUNT;  //updated value
+                Console.WriteLine($"Your balance is now {balance}");
+                                
+                if (BET_AMOUNT > balance)
                 {
                     Console.WriteLine("you do not have enough in your balance for that bet");
                     return;
                 }
-
-
                 int[,] grid = new int[ROWS, COLS];                         //2D array 
 
                 Random rnd = new Random();
@@ -103,8 +117,7 @@ namespace Slot_Machine
                         {
                             win = false;
                             Console.WriteLine("middle row lose");
-                            //Console.WriteLine("Please select a mode for your next spin");
-                            //Console.WriteLine($"Your balance is now {balance - betAmount}");
+                            //Console.WriteLine("Please select a mode for your next spin");                            
                             break;                            
                         }                        
                     }
@@ -118,8 +131,7 @@ namespace Slot_Machine
                             if (grid[i, 0] != grid[i, j]) //&& grid[i, 1] == grid[i, 2])   //checking for values are the same                                                   
                             {
                                 win = false;
-                                Console.WriteLine($"You lose on horizontal line {i + 1}!");                                
-                                //Console.WriteLine($"Your balance is now {balance - betAmount}");
+                                Console.WriteLine($"You lose on horizontal line {i + 1}!");                              
                                 break;
                                 Console.WriteLine("Please select a mode for your next spin");
                             }
@@ -135,8 +147,7 @@ namespace Slot_Machine
                             if (grid[j, i] != grid[0, i])         //checking for values are the same                                                   
                             {
                                 win = false;
-                                Console.WriteLine($"You LOSE on vertical line {i + 1}!");
-                                //Console.WriteLine($"Your balance is now {balance - betAmount}");
+                                Console.WriteLine($"You LOSE on vertical line {i + 1}!");                                
                                 break;
                             }
                         }
@@ -150,15 +161,9 @@ namespace Slot_Machine
                         if (grid[0, 0] != grid[i, i])                            //checking for values are the same                                                   
                         {
                             win = false;
-                            Console.WriteLine("You lose on line 1");
-                            //Console.WriteLine($"Your balance is now {balance - betAmount}");
+                            Console.WriteLine("You lose on line 1");                            
                             break;
-
-                        }
-                        //if (win)
-                        //{
-                        //    Console.WriteLine("You win on line 1");
-                        //}
+                        }                        
                     }
                     //bool winLine2 = true;
                     for (int i = 0; i < ROWS; i++)             //0,2 1,1 2,0  line i need to find   //TOP RIGHT TO BOTTOM LEFT 
@@ -168,11 +173,7 @@ namespace Slot_Machine
                             win = false;
                             Console.WriteLine("You lose on line 2");
                             break;
-                        }
-                        //if (win)
-                        //{
-                        //    Console.WriteLine("You win on line 2");
-                        //}
+                        }                      
                     }
                 }   
                 //if (win = false) 
@@ -180,20 +181,14 @@ namespace Slot_Machine
                 //    Console.WriteLine("Please select a mode for your next spin");
                 //}
 
-                if (win)  //else                    //change to else?
+                if (win)                    //change to else?
                 {
                     int winnings = BET_AMOUNT + PAYOUT;
-                    INITIAL_BALANCE = INITIAL_BALANCE + winnings;//updated value                                       
-                    Console.WriteLine($"YOU WIN,you have won £{PAYOUT}, your new balance is £{INITIAL_BALANCE}");
-                    return;
-                    //Console.WriteLine($"You have won {winnings}");
-                    //output winnings, need calculation
-                }
-                //else 
-                //{
-                //    Console.WriteLine($"you lose, your balance is {balance - betAmount}");                
-                //}
-                if (INITIAL_BALANCE == 0)
+                    balance = balance + winnings;//updated value                                       
+                    Console.WriteLine($"YOU WIN,you have won £{PAYOUT}, your new balance is £{balance}");
+                    return;                    
+                }                
+                if (balance == 0)
                 {
                     Console.WriteLine("Sorry, you do not have any credit left to continue");
                     break;
@@ -203,6 +198,10 @@ namespace Slot_Machine
         }
     }
 }
+
+
+
+
 //update balance on lose senario within each mode 
 //compare update balance between modes to see if this can be done generically 
 //consider/calculate win lose amount
